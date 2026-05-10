290d0879caeca00afd125f64b088c65719d5a5c5
Diego <di.berriosm@duocuc.cl>
2026-05-10 13:34:02 -0400

    Implementado exception en usuario-service

--- usuario-service/src/main/java/com/reservacanchas/cl/usuario_service/service/UsuarioService.java
+++ usuario-service/src/main/java/com/reservacanchas/cl/usuario_service/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package com.reservacanchas.cl.usuario_service.service;
 
 import com.reservacanchas.cl.usuario_service.dto.UsuarioDTO;
+import com.reservacanchas.cl.usuario_service.exception.BadRequestException;
+import com.reservacanchas.cl.usuario_service.exception.ResourceNotFoundException;
 import com.reservacanchas.cl.usuario_service.model.Usuario;
 import com.reservacanchas.cl.usuario_service.repository.UsuarioRepository;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +20,11 @@ public class UsuarioService {
     }
 
     public Usuario guardar(UsuarioDTO usuarioDTO) {
+
+        if (usuarioDTO.getEmail() == null || usuarioDTO.getEmail().isBlank()) {
+            throw new BadRequestException("El email es obligatorio");
+        }
+
         Usuario usuario = new Usuario();
         usuario.setNombre(usuarioDTO.getNombre());
         usuario.setApellido(usuarioDTO.getApellido());
@@ -25,6 +33,7 @@ public class UsuarioService {
         usuario.setTelefono(usuarioDTO.getTelefono());
         usuario.setEstado(usuarioDTO.getEstado());
         usuario.setIdRol(usuarioDTO.getIdRol());
+
         return usuarioRepository.save(usuario);
     }
 
@@ -34,10 +43,12 @@ public class UsuarioService {
 
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Usuario no encontrado"));
     }
 
     public Usuario actualizar(Long id, UsuarioDTO usuarioDTO) {
+
         Usuario usuario = buscarPorId(id);
 
         usuario.setNombre(usuarioDTO.getNombre());
@@ -52,7 +63,9 @@ public class UsuarioService {
     }
 
     public void eliminar(Long id) {
+
         Usuario usuario = buscarPorId(id);
+
         usuarioRepository.delete(usuario);
     }
 
