9ef560f9ec10a13a560dc23f4debf31e229862ae
Oscar <Oa.munoz@duocuc.cl>
2026-06-12 01:05:28 -0400

    Swagger auth-service

--- auth-service/src/main/java/com/reservacanchas/cl/auth_service/config/SwaggerConfig.java
+++ auth-service/src/main/java/com/reservacanchas/cl/auth_service/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.reservacanchas.cl.usuario_service.config;
+package com.reservacanchas.cl.auth_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -12,8 +12,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Usuario")
+                        .title("API Auth")
                         .version("1.0")
-                        .description("Documentación del microservicio Usuario"));
+                        .description("Documentación del microservicio Auth"));
     }
 }
