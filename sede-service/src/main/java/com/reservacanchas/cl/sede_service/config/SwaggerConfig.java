7845c8384dd589a2cdfbc30b581160cafdfa5645
Diego <di.berriosm@duocuc.cl>
2026-06-14 16:03:26 -0400

    JWT SWAGGER SEDE-SERVICE

--- sede-service/src/main/java/com/reservacanchas/cl/sede_service/config/SwaggerConfig.java
+++ sede-service/src/main/java/com/reservacanchas/cl/sede_service/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.reservacanchas.cl.sede_service.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
@@ -10,6 +12,12 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
